e7064f47ef4ddf05c395764b5f60fdf2e1c45219
salahsamy <salahsamy512@gmail.com>
2021-04-12 01:20:32 +0200

    init commit

---
+++ app/src/main/java/com/example/walletapp/Home/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.example.walletapp.Home
+
+import androidx.fragment.app.Fragment
+import com.example.walletapp.R
+
+class HomeFragment : Fragment(R.layout.home_fragment) {
+}
---
+++ app/src/main/java/com/example/walletapp/Intro/IntroFragment.kt
@@ -0,0 +1,7 @@
+package com.example.walletapp.Intro
+
+import androidx.fragment.app.Fragment
+import com.example.walletapp.R
+
+class IntroFragment : Fragment(R.layout.intro_fragment) {
+}
